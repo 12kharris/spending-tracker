abe88c4e4faf1071d0eb86325985c55f48178656
Kurt Harris <kurtharris1541@gmail.com>
2024-06-15 11:17:32 +0000

    Add Category and Transaction models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200)
+
+
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
+    reference = models.CharField(max_length=30, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tracker_transaction"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT ,related_name = "tracker_transaction"
+    )
+    transaction_date = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-transaction_date"]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount} | {self.reference} | {self.category} | {self.transaction_date}"
