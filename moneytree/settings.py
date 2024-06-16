52c8b747e4e602f58967eeb4e9e0e38a2c7b367f
Kurt Harris <kurtharris1541@gmail.com>
2024-06-16 15:24:51 +0000

    view transactions from db

--- moneytree/settings.py
+++ moneytree/settings.py
@@ -21,6 +21,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print(TEMPLATES_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'moneytree.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.db import connection
+from .models import Category, Transaction
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello tracker!")
+    transactions = Transaction.objects.all().order_by("transaction_date")
+
+    return render(
+        request,
+        "tracker/dashboard.html",
+        {
+            "transactions": transactions
+        }
+    )
