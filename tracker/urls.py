c2183470bfb5f5a7ffa82b12621aeb03b2429e95
Kurt Harris <kurtharris1541@gmail.com>
2024-06-16 17:11:51 +0000

    Add home page view

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.home, name="dashboard")
+    path("", views.home, name="home"),
+    path("dashboard/", views.dashboard, name="dashboard")
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.db import connection
 from .models import Category, Transaction
 
 # Create your views here.
 def home(request):
-    transactions = Transaction.objects.all().order_by("transaction_date")
+    return HttpResponse("home")
+
+
+def dashboard(request):
+    transactions = Transaction.objects.filter(user=request.user).order_by("transaction_date")
 
     return render(
         request,
